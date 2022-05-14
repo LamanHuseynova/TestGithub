6184fad8daa31123d4a077aed6d080a45c7d103a
Aytan <ayten.su@gmail.com>
2022-05-13 19:49:18 -0500

    jbjt

---
+++ src/com/company/GitPracticeClass.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class GitPracticeClass {
+    public static void main(String[] args) {
+      practiceClass();
+
+        }
+        public static void practiceClass(){
+            System.out.println("Aytan's Branch");
+    }
+}
